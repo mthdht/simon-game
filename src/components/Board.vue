268f1390f3d456fc60b6ec5e4bf0cfb67c3b984c
mthdht <mthdht@gmail.com>
2018-08-13 13:07:29 +0200

    made app component and start control section on board component

---
+++ src/components/Board.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="board">
+        <div class="controls w3-blue-gray w3-center w3-padding-24">
+            <button class="w3-button w3-round-xxlarge w3-large w3-green w3-hover-blue w3-margin-right"><b>Facile</b></button>
+            <button class="w3-button w3-round-xxlarge w3-large w3-green w3-hover-blue"><b>Restart</b></button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Board"
+    }
+</script>
+
+<style scoped>
+
+</style>
